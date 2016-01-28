a1116a76618f846c3d66935eafb404e05248f6d3
aquere <aquere@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-28 09:07:00 +0000

    refacto des nom de classe et poursuite du code sur connexion

---
+++ AdopteUnDevApiUc/src/main/java/fr/afcepf/adopteundev/uc/api/IUcUtilisateur.java
@@ -0,0 +1,7 @@
+package fr.afcepf.adopteundev.uc.api;
+
+/**
+ * Created by Stagiaire on 28/01/2016.
+ */
+public class IUcUtilisateur {
+}
---
+++ AdopteUnDevBusinessImpl/src/main/java/fr/afcepf/adopteundev/business/impl/BusinessUtilisateurImpl.java
@@ -0,0 +1,20 @@
+package fr.afcepf.adopteundev.business.impl;
+
+import assembleur.EntityToDTO;
+import dto.DTOUtilisateur;
+import entity.Utilisateur;
+import fr.afcepf.adopteundev.api.utilisateur.IDaoUtilisateur;
+import fr.afcepf.adopteundev.business.api.IBusinessUtilisateur;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class BusinessUtilisateurImpl implements IBusinessUtilisateur {
+    private IDaoUtilisateur daoUtilisateur;
+
+    @Override
+    public DTOUtilisateur connecterDtoUtilisateur(String mail, String mdp) {
+        Utilisateur utilisateur = daoUtilisateur.seConnecter(mail, mdp);
+        return EntityToDTO.utilisateurToDTOUtilisateur(utilisateur);
+    }
+}
