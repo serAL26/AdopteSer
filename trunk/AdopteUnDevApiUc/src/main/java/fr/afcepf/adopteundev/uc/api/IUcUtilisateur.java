d115bb4e3494f314c00b4a1fa90fcdbc646bf42e
aquere <aquere@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-28 09:34:19 +0000

    coherence : refacto package

--- trunk/AdopteUnDevApiUc/src/main/java/fr/afcepf/adopteundev/uc/api/IUcUtilisateur.java
+++ trunk/AdopteUnDevApiUc/src/main/java/fr/afcepf/adopteundev/uc/api/IUcUtilisateur.java
@@ -1,7 +1,7 @@
 package fr.afcepf.adopteundev.uc.api;
 
-/**
- * Created by Stagiaire on 28/01/2016.
- */
-public class IUcUtilisateur {
+import dto.DTOUtilisateur;
+
+public interface IUcUtilisateur {
+    DTOUtilisateur toConnect(String mail,String mdp);
 }
