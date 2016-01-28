4aa9c87edde4588f501364d977eee8ccfc87aab2
YasmineZRELLI <YasmineZRELLI@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-28 10:34:47 +0000

    typeAppli et typeService dans les DAO

---
+++ trunk/AdopteUnDevDaoImpl/src/main/java/fr/afcepf/adopteundev/projet/DaoTypeAppli.java
@@ -0,0 +1,22 @@
+package fr.afcepf.adopteundev.projet;
+
+import java.util.List;
+
+import entity.TypeAppli;
+import fr.afcepf.adopteundev.idao.projet.IDaoTypeAppli;
+
+public class DaoTypeAppli implements IDaoTypeAppli {
+
+	@Override
+	public List<TypeAppli> getAllApplis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TypeAppli> getAppliById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ trunk/AdopteUnDevDaoImpl/src/main/java/fr/afcepf/adopteundev/projet/DaoTypeService.java
@@ -0,0 +1,29 @@
+package fr.afcepf.adopteundev.projet;
+
+import java.util.List;
+
+import entity.TypeAppli;
+import entity.TypeService;
+import fr.afcepf.adopteundev.idao.projet.IDaoTypeService;
+
+public class DaoTypeService implements IDaoTypeService {
+
+	@Override
+	public List<TypeService> getAllServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TypeService> getSericesById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TypeService> getServicesByAppli(TypeAppli appli) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
