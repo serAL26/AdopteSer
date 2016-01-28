77bbe0d31e3d0b2595dcde9299ac431558979d40
aquere <aquere@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-28 08:52:32 +0000

    generation des packages et début du dev de la fonctionnalité connexion

---
+++ trunk/AdopteUnDevDaoImpl/src/main/java/fr/afcepf/adopteundev/utilisateurImpl/DaoUtilisateurImpl.java
@@ -0,0 +1,28 @@
+package fr.afcepf.adopteundev.utilisateurImpl;
+
+import entity.Utilisateur;
+import fr.afcepf.adopteundev.api.utilisateur.IDaoUtilisateur;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Stateless
+public class DaoUtilisateurImpl implements IDaoUtilisateur{
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public Utilisateur seConnecter(String mail, String password) {
+        TypedQuery<Utilisateur> query = em.createQuery("FROM Utilisateur u WHERE u.mail like :email AND u.mdp like :password", Utilisateur.class)
+                .setParameter("email",mail)
+                .setParameter("password",password);
+        List<Utilisateur> list = query.getResultList();
+        if (list.size()==1)
+            return list.get(0);
+        else
+            return null;
+    }
+}
