9fc10f30468eb538e724cbb070cbddd6a7a5956b
aquere <aquere@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-29 10:17:31 +0000

    correction pom et ucName

--- trunk/AdopteUnDevWeb/src/main/java/fr/afcepf/adopteundev/managedbean/gestioncdc/MBAjoutCdc.java
+++ trunk/AdopteUnDevWeb/src/main/java/fr/afcepf/adopteundev/managedbean/gestioncdc/MBAjoutCdc.java
@@ -1,93 +1,91 @@
 package fr.afcepf.adopteundev.managedbean.gestioncdc;
 
-import java.util.Date;
-
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
 import dto.DTOCdc;
+import dto.DTODeveloppeur;
 import dto.DTOProjet;
 import dto.DTOTypeCdc;
 import fr.afcepf.adopteundev.gestion.cdc.IUCGestionCdc;
 import fr.afcepf.adopteundev.managedbean.util.ContextFactory;
 import fr.afcepf.adopteundev.managedbean.util.UcName;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Date;
+
 @ManagedBean
 @SessionScoped
 public class MBAjoutCdc {
-	
-	private String besoin;
-	private String contexte;
-	private String existant;
-	private Date dateFin;
-	private Double tarif;
-	
-	private DTOCdc cdc;
-	
-	@EJB
-	private IUCGestionCdc gestionCdc;
-	
-	@PostConstruct
-	private void obtenirLesInterfaces()
-	{
-		 gestionCdc = (IUCGestionCdc) ContextFactory.createProxy(UcName.UCGESTIONCDC);
-	}
 
-	public String getBesoin() {
-		return besoin;
-	}
+    private String besoin;
+    private String contexte;
+    private String existant;
+    private Date dateFin;
+    private Double tarif;
 
-	public void setBesoin(String besoin) {
-		this.besoin = besoin;
-	}
+    private DTOCdc cdc;
 
-	public String getContexte() {
-		return contexte;
-	}
+    @EJB
+    private IUCGestionCdc gestionCdc;
 
-	public void setContexte(String contexte) {
-		this.contexte = contexte;
-	}
+    @PostConstruct
+    private void obtenirLesInterfaces() {
+        gestionCdc = (IUCGestionCdc) ContextFactory.createProxy(UcName.UCGESTIONCDC);
+    }
 
-	public String getExistant() {
-		return existant;
-	}
+    public String getBesoin() {
+        return besoin;
+    }
 
-	public void setExistant(String existant) {
-		this.existant = existant;
-	}
+    public void setBesoin(String besoin) {
+        this.besoin = besoin;
+    }
 
-	public Date getDateFin() {
-		return dateFin;
-	}
+    public String getContexte() {
+        return contexte;
+    }
 
-	public void setDateFin(Date dateFin) {
-		this.dateFin = dateFin;
-	}
+    public void setContexte(String contexte) {
+        this.contexte = contexte;
+    }
 
-	public Double getTarif() {
-		return tarif;
-	}
+    public String getExistant() {
+        return existant;
+    }
 
-	public void setTarif(Double tarif) {
-		this.tarif = tarif;
-	}
+    public void setExistant(String existant) {
+        this.existant = existant;
+    }
 
-	public DTOCdc getCdc() {
-		return cdc;
-	}
+    public Date getDateFin() {
+        return dateFin;
+    }
 
-	public void setCdc(DTOCdc cdc) {
-		this.cdc = cdc;
-	}
-	
-	public void ajouterCdc()
-	{
-		cdc = new DTOCdc(contexte, besoin, existant, tarif, dateFin, new DTOProjet(), new DTOTypeCdc(), null);
-		
-		gestionCdc.ajouterCdcDto(cdc);
-	}
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Double getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(Double tarif) {
+        this.tarif = tarif;
+    }
+
+    public DTOCdc getCdc() {
+        return cdc;
+    }
+
+    public void setCdc(DTOCdc cdc) {
+        this.cdc = cdc;
+    }
+
+    public void ajouterCdc() {
+        cdc = new DTOCdc(contexte, besoin, existant, tarif, dateFin, new DTOProjet(), new DTOTypeCdc(), new DTODeveloppeur());
+
+        gestionCdc.ajouterCdcDto(cdc);
+    }
 
 }
