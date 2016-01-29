4374c83557ca260c72f80f8b2e26cfa392d1e22a
aquere <aquere@ce721e46-eb91-45aa-8e40-671618bd1565>
2016-01-29 10:04:30 +0000

    correction pom et ucName

--- trunk/AdopteUnDevWeb/src/main/java/fr/afcepf/adopteundev/managedbean/util/UcName.java
+++ trunk/AdopteUnDevWeb/src/main/java/fr/afcepf/adopteundev/managedbean/util/UcName.java
@@ -1,9 +1,9 @@
 package fr.afcepf.adopteundev.managedbean.util;
 
 public class UcName {
-	public static final String UCCONNEXION = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl-0.0.1-SNAPSHOT/UcConnexion!fr.afcepf.adopteundev.gestion.connexion.IUcConnexion";
-	public static final String UCGESTIONCDC = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl-0.0.1-SNAPSHOT/UcGestionCdcImpl!fr.afcepf.adopteundev.gestion.cdc.IUCGestionCdc";
-	public static final String UCGESTIONMESSAGERIE = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl-0.0.1-SNAPSHOT/UCMessage!fr.afcepf.adopteundev.message.IUCMessage";
-	public static final String UCGESTIONPROJET = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl-0.0.1-SNAPSHOT/UCProjet!fr.afcepf.adopteundev.gestion.projet.IUCProjet";
+	public static final String UCCONNEXION = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl/UcConnexion!fr.afcepf.adopteundev.gestion.connexion.IUcConnexion";
+	public static final String UCGESTIONCDC = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl/UcGestionCdcImpl!fr.afcepf.adopteundev.gestion.cdc.IUCGestionCdc";
+	public static final String UCGESTIONMESSAGERIE = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl/UCMessage!fr.afcepf.adopteundev.message.IUCMessage";
+	public static final String UCGESTIONPROJET = "ejb:AdopteUnDevEAR/AdopteUnDevUcImpl/UCProjet!fr.afcepf.adopteundev.gestion.projet.IUCProjet";
 	public static final String test = "sfdml";
 }
